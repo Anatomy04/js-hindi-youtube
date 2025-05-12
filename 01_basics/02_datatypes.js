19797de010a79efd1062ddba1195112746068294
krishankant <Kksingh2228@gmail.com>
2025-05-12 13:57:11 +0000

    datatypes and conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodeJS, not browser
+
+let name = "kk"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "33abc"
+// let score = null\
+// let score = undefined
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "something" => true
+
+let someNumber = 33
+let someString = String(someNumber)
+console.log(someString)
+console.log(typeof someString)
