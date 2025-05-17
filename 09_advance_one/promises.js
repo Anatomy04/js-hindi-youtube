65f720394643f081303f7579897880c880df83f2
krishankant <Kksingh2228@gmail.com>
2025-05-17 08:03:24 +0000

    promises creation,, consumption, async method and fetch

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,3 +1,6 @@
+
+// ********************1*******************
+
 const promiseOne = new Promise(function(resolve, reject) {
     // Do an async task
     // DB calss, cryptography, network
@@ -12,12 +15,101 @@ promiseOne.then(function(){
     
 })
 
+
+// *******************2********************
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log('Async task 2')
         resolve()
-    }, 2000)
+    }, 1000)
 
 }).then(function(){
-    console.log("Asyn 2 resolved")
+    console.log("Asyn 2 resolved");
 })
+
+
+// **********************3**************************
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function() {
+        resolve({username: "kk", emial: "xyz@gmail.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+// *********************4***************************
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error) {
+            resolve({username: "kk", password: "123"})
+        }
+        else {
+            reject("ERROR: something is wrong")
+        }
+    }, 1000)
+})
+
+// promiseFour.then((user) => {
+//     console.log(user);
+//     return user.username
+// }).then((username) => {
+//     console.log(username);
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+promiseFour
+  .then(user => console.log(user) || user.username)
+  .then(username => console.log(username))
+  .catch(error => console.log(error))
+  .finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
+//   ***************5****************
+const promiseFive = new Promise((resolve, reject) =>
+    setTimeout(function(){
+        let error = true
+        if(!error) {
+            resolve({username: "javascript", password: "123"})
+        }
+        else {
+            reject("ERROR: javascript went wrong")
+        }
+    }, 1000)
+)
+
+async function consumePromiseFive() {
+    try {
+        const response_1 = await promiseFive
+        console.log(response);
+    }   catch (error) {
+            console.log(error)
+    }
+}
+
+consumePromiseFive()
+
+
+
+
+// async function getAllUsers() {
+//     try {
+//         const response_2 = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response_2.json()
+//         console.log(data);
+//     }   catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => response.json())
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
+
